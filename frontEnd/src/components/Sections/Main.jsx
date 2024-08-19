70fe94f5b059d6d32110815ada779e8f1a23a4df
torikallcode <torikalcontent@gmail.com>
2024-08-20 06:44:31 +0800

    update responsive

--- frontEnd/src/components/Sections/Main.jsx
+++ frontEnd/src/components/Sections/Main.jsx
@@ -63,17 +63,17 @@ export const Main = () => {
       <h1 className='text-3xl font-utama text-raven2 font-bold mb-7'>Statistik Data</h1>
       <div className='grid grid-cols-1 lg:grid-cols-2 gap-7 justify-center gap-x-7 mb-16 lg:mx-auto'>
         <div className='py-5 rounded-xl bg-[#609966] w-full h-72 flex justify-center items-center relative'>
-          <h1 className='text-white font-utama text-lg absolute top-5 left-5'>Tingkat kelembaban</h1>
+          <h1 className='text-white font-utama lg:text-lg absolute top-5 left-5'>Tingkat kelembaban</h1>
           <SemiCircularProgressBar progress={latestData.kelembaban} />
         </div>
         <div className='flex justify-between items-start gap-x-5 rounded-xl w-full h-32 relative lg:h-full'>
           <div className='flex justify-center relative items-center w-full h-full rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 bg-gray-900 px-2'>
-            <h1 className='font-utama text-raven2 absolute top-3 left-3'>Status</h1>
-            <h2 className='text-lg font-medium text-center font-utama text-raven2'>{latestData.status}</h2>
+            <h1 className='font-utama text-raven2 absolute top-3 left-3 lg:top-5 lg:left-5 lg:text-lg'>Status</h1>
+            <h2 className='text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl font-medium text-center font-utama text-raven2'>{latestData.status}</h2>
           </div>
           <div className='bg-gray-900 flex justify-center relative items-center w-full h-full rounded-xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 px-2'>
-            <h1 className='font-utama text-raven2 absolute top-3 left-3'>Time</h1>
-            <h2 className='text-md font-medium text-center font-utama text-raven2'>{latestData.waktu}</h2>
+            <h1 className='font-utama text-raven2 absolute top-3 left-3 lg:top-5 lg:left-5 lg:text-lg'>Time</h1>
+            <h2 className='text-sm sm:text-lg md:text-lg lg:text-xl xl:text-2xl xl:px-7 font-medium text-center font-utama text-raven2'>{latestData.waktu}</h2>
           </div>
         </div>
       </div>
